e13d7678937b49334e6cafb999adbb704b99f5bd
Marc Lammers <marc@lammers.dev>
2022-07-23 07:09:44 +0200

    Buttons added to main page

--- src/ViewModels/FileTrackerViewModel.ts
+++ src/ViewModels/FileTrackerViewModel.ts
@@ -1,1 +1,14 @@
-export class FileTrackerViewModel {}
+export class FileTrackerViewModel {
+  lockSync() {
+    console.info('lockSync')
+  }
+  unlockSync() {
+    console.info('unlockSync')
+  }
+  syncFiles() {
+    console.info('syncFiles')
+  }
+  startProgram() {
+    console.info('startProgram')
+  }
+}
